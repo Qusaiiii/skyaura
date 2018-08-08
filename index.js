39f25da753ccbbe9713b0e1674f16164db6b0bd2
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-08 21:27:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -113,8 +113,11 @@ client.on('message', message => {
               }else {
                 return message.reply(":x: " + "| You need to have the \"ADMINISTRATOR\" Permission")
                       message.channel.send(mmmmEmbed)
-       };
-}
+        });
+
+    });
+
+});
 client.on('message', message => {
    if(message.content.startsWith(prefix + "invites")) {
     message.guild.fetchInvites().then(invs => {
@@ -12495,6 +12498,3 @@ message.member.addRole(message.guild.roles.find("name", "100"));
 });
   
 client.login('NDc2MDA4OTI5MzExMzI2MjA5.DknXIw.QxwGHc-6iDoMB1If6zUGIIgXk8Q');
-
-
-	});
